f531bf7cdb1206155fd8e2d9ee73bf1111a06b49
Ebener <Jonathan@ebener.net>
2024-11-17 16:15:20 +0100

    added a simple pause menu

--- st2/src/Game.cpp
+++ st2/src/Game.cpp
@@ -3,26 +3,45 @@
 #include "Button.h"
 #include "Eventsystem.h"
 #include "LayerManager.h"
+#include "PauseMenu.h"
 #include "Utils/Log.h"
+
+Game::Game(int level_id)
+{
+}
 
 void Game::update(std::shared_ptr<Eventsystem>& eventsystem, std::shared_ptr<LayerManager>& layer_manager, std::shared_ptr<Soundsystem>& soundsystem, sf::RenderWindow& window, double deltatime)
 {
 	if(eventsystem->get_key_action(sf::Keyboard::Key::Escape) == Eventsystem::action_pressed)
 	{
-		layer_manager->pop_layer();
-		//layer_manager->push_layer(std::make_shared<PausenMenu>()); 
+		layer_manager->push_layer(std::make_shared<PauseMenu>(layer_manager->get_top())); 
 		return;
 	}
+
+	if(eventsystem->get_key_action(sf::Keyboard::Key::Q) == Eventsystem::action_pressed)
+	{
+		layer_manager->pop_layer(); 
+		return;
+	}
+
+	std::shared_ptr<int> int1 = std::make_shared<int>(1);
+	int* int2 = int1.get();
+	std::shared_ptr<int> int3 (int2);
+
+
 }
 
 void Game::render(sf::RenderWindow& window)
 {
-
+	sf::RectangleShape shape(sf::Vector2f(window.getSize()));
+	shape.setPosition(0, 0);
+	shape.setFillColor(sf::Color::Cyan);
+	window.draw(shape);
 }
 
 void Game::on_close()
 {
-	
+	LOG_INFO("closed game");
 }
 
 LayerID Game::get_layer_id()
